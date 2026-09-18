d936cd80356d8c66a0dbbd4eca47e4ae77b4a6a4
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-09-18 13:05:24 +0300

    Add PWA sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE='afrn-league-v3';
+const APP_SHELL=['./','./index.html','./style.css','./app.js','./competition.html','./competition.js','./competition-stats.js','./match.html','./match.js','./match-stats.js','./match-shots.js','./team.html','./team.js','./player.html','./player.js','./scorers.html','./scorers.js','./admin.html','./admin.js','./centre-pro.js','./auth.html','./auth.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin)return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+  );
+});
