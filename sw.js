4be15b4e4885ba5bc5e1f76fe09d027b6759deae
enyonyiabemba94-eng <enyonyiabemba94@gmail.com>
2026-09-18 16:50:49 +0300

    Bump PWA cache for reliable homepage sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='afrn-league-v5';
+const CACHE='afrn-league-v6';
 const APP_SHELL=['./','./index.html','./style.css','./app.js','./competition.html','./competition.js','./competition-stats.js','./match.html','./match.js','./match-stats.js','./match-shots.js','./team.html','./team.js','./player.html','./player.js','./scorers.html','./scorers.js','./admin.html','./admin.js','./centre-pro.js','./auth.html','./auth.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
